362ee4ad1a58517001984da8655b176e704864a0
Marek <m.bojarovic@gmail.com>
2017-01-15 16:59:01 +0200

    урок 2. Empty test file

--- test.php
+++ test.php
@@ -1,22 +1,3 @@
 <?php
 
 require __DIR__ . '/autoload.php';
-
-class mySingleton
-{
-    public $time;
-
-    public function __construct()
-    {
-        $this->time = microtime(true);
-    }
-}
-
-$obj = new mySingleton();
-echo $obj->time;
-
-echo '<br>';
-sleep(1);
-
-$obj = new mySingleton();
-echo $obj->time;
