cb14e5706e2721be2fa03131d4c914426c8b199c
Marek <m.bojarovic@gmail.com>
2017-01-21 21:36:09 +0200

    upgrade again

---
+++ App/Models/Controller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marek
+ * Date: 1/21/2017
+ * Time: 7:17 PM
+ */
+
+namespace App\Models;
+
+
+abstract class Controller
+{
+    public function __construct()
+    {
+        
+    }
+}
